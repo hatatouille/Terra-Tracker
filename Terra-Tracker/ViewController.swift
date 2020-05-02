b7d386fce6fa92791656f1e6cc01a12d3f793e25
Hiroki Shiotsuka <hiroki@workhouse.me>
2020-05-02 11:11:00 +0900

    Initial Commit

---
+++ Terra-Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Terra-Tracker
+//
+//  Created by 塩塚 弘樹 on 2020/05/02.
+//  Copyright © 2020 塩塚 弘樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
